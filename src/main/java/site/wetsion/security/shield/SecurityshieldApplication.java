738568e3de75fac5834e96cb967efb6775818fee
Wetsion <13110018525@163.com>
2020-01-09 00:11:52 +0800

    init

---
+++ src/main/java/site/wetsion/security/shield/SecurityshieldApplication.java
@@ -0,0 +1,13 @@
+package site.wetsion.security.shield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityshieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityshieldApplication.class, args);
+	}
+
+}
---
+++ src/main/java/site/wetsion/security/shield/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package site.wetsion.security.shield.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ *
+ * Created by wetsion on 2020/1/8.
+ */
+@EnableWebSecurity
+public class SecurityConfig {
+}
